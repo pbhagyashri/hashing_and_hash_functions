cfdfe032fb320614354054b7f39271f21398f416
pbhagyashri <bhagyashri.padmawar@gmail.com>
2018-07-08 21:56:04 -0400

    Add code to check if any 2 movie's comabined length is less that flight length

--- inflight_entertainment.rb
+++ inflight_entertainment.rb
@@ -1,3 +1,6 @@
+require 'set'
+require 'pry'
+
 #Write a method that takes an integer flight_length (in minutes) and an array of integers movie_length(in minutes) and returns a boolean indicating whether there are 2 numbers in movie_length array whoes sum equals to flight_length.
 
 # Assume that user will watch 2 movies
@@ -10,3 +13,27 @@
 
 #approach 2 - replacing the inner for loop with a set to get a constant time lookup. 
 #through all movie lengths in a set first in O(n) time.then loop through our possible first_movie_lengths and replace our inner loop with a simple check in our set. this will give O(n) runtime overall. 
+
+def can_two_movies_fill_flight(movie_lengths, flight_length) 
+
+  #movie lengths we've seen so far
+
+  movie_lengths_seen = Set.new
+
+  movie_lengths.each do |first_movie_length|
+    matching_second_movie_length = flight_length - first_movie_length
+
+    if movie_lengths_seen.include?(matching_second_movie_length)
+      return true
+    end
+
+    movie_lengths_seen.add(first_movie_length)
+  end
+
+  #we never found a match so return false
+  return false
+end
+
+puts can_two_movies_fill_flight([90, 100, 120], 180)
+
+#We know users wont watch the same movie again because we have checked if movie_lengths_seen for matching_second_movie_length before we have added first_movie_length in it.
